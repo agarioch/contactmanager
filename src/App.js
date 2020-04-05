7e1363ecce2be90f16472c8ef9721a743ef72fd5
alistair garioch <alistairgarioch@gmail.com>
2020-04-05 21:00:11 +0100

    created header and contact components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+import './App.css';
+import { render } from '@testing-library/react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe"
+          email="jdoe@gmail.com"
+          phone="555-555-5555" />
+        <Contact name="Karen Smith"
+          email="karen@gmail.com"
+          phone="333-333-3333" />
+      </div>
+    );
+  }
 }
 
 export default App;
