98a92b5ea62af786663a1c6c659e0d31395a2aa3
alistair garioch <alistairgarioch@gmail.com>
2020-04-11 21:48:56 +0100

    Updates

---
+++ precache-manifest.bd7f0d1f858fdb8a846bca82bbc5abe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9f12b727bd43b1de1d1d741fe8f01d",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "496c129d490be7c223af",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c191482bed1fa6e3f6fb",
+    "url": "/contactmanager/static/css/main.e70250b5.chunk.css"
+  },
+  {
+    "revision": "496c129d490be7c223af",
+    "url": "/contactmanager/static/js/2.e9b5a9b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.e9b5a9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c191482bed1fa6e3f6fb",
+    "url": "/contactmanager/static/js/main.72cd1e15.chunk.js"
+  },
+  {
+    "revision": "2b43dc7d0ef5c78fcd0f",
+    "url": "/contactmanager/static/js/runtime-main.08b2bbf7.js"
+  }
+]);
